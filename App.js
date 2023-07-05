632af066a039f0d872b867a41d40a7abbf334e64
Lea Papadopoulos <92717379+LeaPapadopoulos@users.noreply.github.com>
2023-07-06 00:11:48 +0300

    Initialize Firebase and Firestore

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+// Initialize Firebase and Firestore
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 // import the screens we want to navigate
 import Start from "./components/Start";
 import Chat from "./components/Chat";
@@ -10,11 +14,26 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDchaPzF-tgENZ6faXUGL1JceM9DWBHRbg",
+    authDomain: "lea-chat-app.firebaseapp.com",
+    projectId: "lea-chat-app",
+    storageBucket: "lea-chat-app.appspot.com",
+    messagingSenderId: "937448197819",
+    appId: "1:937448197819:web:f13f5f174a2a7f3d67a39e",
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Start">
         <Stack.Screen name="Start" component={Start} />
-        <Stack.Screen name="Chat" component={Chat} />
+        <Stack.Screen name="Chat">
+          {(props) => <Chat db={db} {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
