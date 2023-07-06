16389c1ac89c81b841df2dbaae22a34005eac858
Lea Papadopoulos <92717379+LeaPapadopoulos@users.noreply.github.com>
2023-07-07 00:24:01 +0300

    utilize the isConnected prop to decide what source to fetch data from

--- components/Chat.js
+++ components/Chat.js
@@ -9,6 +9,7 @@ import {
   orderBy,
   Timestamp,
 } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Chat = ({ route, navigation, db }) => {
   const { name, userID } = route.params;
@@ -18,25 +19,73 @@ const Chat = ({ route, navigation, db }) => {
     addDoc(collection(db, "messages"), newMessages[0]);
   };
 
+  let messageList;
+
   useEffect(() => {
     navigation.setOptions({ title: name });
-    const messageList = onSnapshot(
-      query(collection(db, "messages"), orderBy("createdAt", "desc")),
-      (documentsSnapshot) => {
-        let newMessages = [];
-        documentsSnapshot.forEach((doc) => {
-          const createdAt = doc.data().createdAt.toDate(); // Convert Timestamp to Date
-          newMessages.push({ id: doc.id, ...doc.data(), createdAt });
-        });
-        setMessages(newMessages);
-      }
-    );
+
+    if (isConnected === true) {
+      // unregister current onSnapshot() listener to avoid registering multiple listeners when
+      // useEffect code is re-executed.
+      if (messageList) messageList();
+      messageList = null;
+
+      messageList = onSnapshot(
+        query(collection(db, "messages"), orderBy("createdAt", "desc")),
+        async (documentsSnapshot) => {
+          let newMessages = [];
+          documentsSnapshot.forEach((doc) => {
+            const createdAt = doc.data().createdAt.toDate(); // Convert Timestamp to Date
+            newMessages.push({ id: doc.id, ...doc.data(), createdAt });
+          });
+          cacheMessages(newLists);
+          setMessages(newMessages);
+        }
+      );
+    } else loadCachedMessages();
 
     // Clean up code
     return () => {
       if (messageList) messageList();
     };
-  }, []);
+  }, [isConnected]);
+
+  const cacheMessages = async (listsToCache) => {
+    try {
+      await AsyncStorage.setItem("messages-list", JSON.stringify(newMessages));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  let unsubShoppinglists;
+
+  useEffect(() => {
+    if (isConnected === true) {
+      // unregister current onSnapshot() listener to avoid registering multiple listeners when
+      // useEffect code is re-executed.
+      if (unsubShoppinglists) unsubShoppinglists();
+      unsubShoppinglists = null;
+
+      const q = query(
+        collection(db, "shoppinglists"),
+        where("uid", "==", userID)
+      );
+      unsubShoppinglists = onSnapshot(q, (documentsSnapshot) => {
+        let newLists = [];
+        documentsSnapshot.forEach((doc) => {
+          newLists.push({ id: doc.id, ...doc.data() });
+        });
+        cacheShoppingLists(newLists);
+        setLists(newLists);
+      });
+    } else loadCachedLists();
+
+    // Clean up code
+    return () => {
+      if (unsubShoppinglists) unsubShoppinglists();
+    };
+  }, [isConnected]);
 
   return (
     <View style={styles.container} backgroundColor={color}>
